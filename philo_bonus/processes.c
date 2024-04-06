6b42780480d53e41a587fbdaf9ac7eeea05b3ed3
saad-out <outsaad03@gmail.com>
2024-04-06 23:40:09 +0000

    processes.c file norm

--- philo_bonus/processes.c
+++ philo_bonus/processes.c
@@ -6,60 +6,95 @@
 /*   By: soutchak <soutchak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:30:37 by soutchak          #+#    #+#             */
-/*   Updated: 2024/04/06 02:22:53 by soutchak         ###   ########.fr       */
+/*   Updated: 2024/04/06 23:39:02 by soutchak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+static void	create_threads(t_philo *philo, pthread_t *pt, pthread_t *mt)
+{
+	int	ret;
+
+	ret = pthread_create(pt, NULL, philo_thread, (void *)philo);
+	if (ret != 0)
+	{
+		ft_putendl_fd(P_CREATE_ERROR, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	ret = pthread_create(mt, NULL, monitor_thread, (void *)philo);
+	if (ret != 0)
+	{
+		ft_putendl_fd(P_CREATE_ERROR, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	join_threads(pthread_t pt, pthread_t mt, void *ret_value)
+{
+	int	ret;
+
+	ret = pthread_join(mt, &ret_value);
+	if (ret != 0)
+	{
+		ft_putendl_fd(P_JOIN_ERROR, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	ret = pthread_join(pt, NULL);
+	if (ret != 0)
+	{
+		ft_putendl_fd(P_JOIN_ERROR, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
+
 void	start_threads(t_philo *philo)
 {
-	t_program *program;
+	t_program	*program;
 	pthread_t	monitor_t;
 	pthread_t	philo_t;
 	int			ret;
 	void		*ret_value;
 
-	/* init */
 	program = philo->program;
-
-	/* create threads */
-	ret = pthread_create(&philo_t, NULL, philo_thread, (void *)philo);
-	if (ret != 0)
-		perror("pthread_create"), exit(EXIT_FAILURE);
-	ret = pthread_create(&monitor_t, NULL, monitor_thread, (void *)philo);
-	if (ret != 0)
-		perror("pthread_create"), exit(EXIT_FAILURE);
-	
-	/* join monitor */
-	ret = pthread_join(monitor_t, &ret_value);
-	if (ret != 0)
-		perror("pthread_join"), exit(EXIT_FAILURE);
-
-	/* join philo */
-	ret = pthread_join(philo_t, NULL);
-	if (ret != 0)
-		perror("pthread_join"), exit(EXIT_FAILURE);
-	// ret = pthread_detach(philo_t);
-	// if (ret != 0)
-	// 	perror("pthread_join"), exit(EXIT_FAILURE);
-	// printf("%s philo done%s\n", philo->color, RESET);
-
-	/* cleanup */
+	ret_value = NULL;
+	create_threads(philo, &philo_t, &monitor_t);
+	join_threads(philo_t, monitor_t, ret_value);
 	clear_philos(philo->program->philos, philo->program->n_philos, false);
 	if (sem_close(program->var_lock) == -1)
-		perror("sem close 1 prog");
+		ft_putendl_fd(SEM_CLOSE_ERROR, STDERR_FILENO);
 	if (sem_close(program->print_lock) == -1)
-		perror("sem close 1 prog");
+		ft_putendl_fd(SEM_CLOSE_ERROR, STDERR_FILENO);
 	if (sem_close(program->forks) == -1)
-		perror("sem close 1 prog");
+		ft_putendl_fd(SEM_CLOSE_ERROR, STDERR_FILENO);
 	free(program);
-
-	/* exit status */
 	ret = *(int *)ret_value;
 	free(ret_value);
-	// printf("%s************* PHILO %d GOING TO EXIT WITH %d%s\n",color, id, ret, RESET);
 	exit(ret);
+}
+
+static bool	spawn_processes(t_program *program, t_philo **philos)
+{
+	__u_int	i;
+	pid_t	pid;
+
+	i = 0;
+	while (i < program->n_philos)
+	{
+		pid = fork();
+		if (pid == -1)
+			return (ft_putendl_fd("fork() error", STDERR_FILENO), false);
+		else if (pid == 0)
+		{
+			start_threads(program->philos[i]);
+			ft_putendl_fd("start_threads should not return", STDERR_FILENO);
+			exit(EXIT_FAILURE);
+		}
+		else
+			philos[i]->pid = pid;
+		i++;
+	}
+	return (true);
 }
 
 void	start_processes(t_program *program)
@@ -67,30 +102,25 @@ void	start_processes(t_program *program)
 	pid_t	pid;
 	t_philo	**philos;
 	int		status;
+	__u_int	i;
 
 	philos = program->philos;
-	for (__u_int i = 0; i < program->n_philos; i++)
-	{
-		pid = fork();
-		if (pid == -1)
-			return (printf("fork error\n"), (void)0);
-		else if (pid == 0)
-		{
-			start_threads(philos[i]);
-			printf("start_threads should not return\n");
-			exit(EXIT_FAILURE);
-		}
-		else
-			philos[i]->pid = pid;
-	}
-	for (__u_int i = 0; i < program->n_philos; i++)
+	if (!spawn_processes(program, philos))
+		exit(EXIT_FAILURE);
+	i = 0;
+	while (i < program->n_philos)
 	{
 		pid = waitpid(-1, &status, 0);
 		if (pid == -1)
-			perror("waitpid"), exit(EXIT_FAILURE); // TODO: clear resources
+		{
+			ft_putendl_fd("waitpid() error", STDERR_FILENO);
+			kill_all_except(philos, program->n_philos, 0);
+			exit(EXIT_FAILURE);
+		}
 		if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
-			continue;
+			continue ;
 		else
 			return (kill_all_except(philos, program->n_philos, pid));
+		i++;
 	}
 }
